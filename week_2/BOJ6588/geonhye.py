34aba5dfca3c01944c59f6c559d50d4a07be7b6b
qndn3tp <qndn3tp@naver.com>
2024-10-03 03:14:00 +0900

    BOJ6588 / 1h 40m

---
+++ week_2/BOJ6588/geonhye.py
@@ -0,0 +1,31 @@
+# 골드바흐의 추측 / 1h 40m
+import sys
+
+def findPrime(n):
+    prime = [True] * (n + 1)
+    p = 2
+    while p*p <= n:
+        if prime[p] == True:
+            for i in range(2 * p, n+1, p):
+                prime[i] = False
+        p += 1
+    return prime
+
+def main():
+    global prime
+    prime = findPrime(1000001)
+    while True:
+        n = int(sys.stdin.readline().strip())
+        if n == 0:
+            break
+        else:
+            print(solution(n))
+
+def solution(n):
+    for i in range(2, int(n*(1/2))+1):
+        if prime[i] and prime[n-i]:
+            return f"{n} = {i} + {n-i}"
+    return "Goldbach's conjecture is wrong."
+
+if __name__ == "__main__":
+    main()
