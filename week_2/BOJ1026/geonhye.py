d228cc040663ba357d21533071818e4c153cc58e
qndn3tp <qndn3tp@naver.com>
2024-10-03 03:13:33 +0900

    BOJ1026 / 25m

---
+++ week_2/BOJ1026/geonhye.py
@@ -0,0 +1,18 @@
+# 보물 / 25m
+import sys
+
+def main():
+    N = int(sys.stdin.readline().strip())
+    A = list(map(int, sys.stdin.readline().strip().split()))
+    B = list(map(int, sys.stdin.readline().strip().split()))
+
+    A.sort()
+
+    res = 0
+    for i in range(N):
+        res += A[i] * max(B)
+        B.remove(max(B))
+    print(res)
+
+if __name__ == "__main__":
+    main()
