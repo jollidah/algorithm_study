972a05a61a102a48c9bb26cc0bd78aee6143fe6b
qndn3tp <qndn3tp@naver.com>
2024-10-31 19:34:52 +0900

    Week_3 / BOJ11055 / ?

--- week_3/BOJ11055/geonhye.py
+++ week_3/BOJ11055/geonhye.py
@@ -6,6 +6,17 @@ input = sys.stdin.readline
 def main(): 
     n = int(input().strip())
     ls = list(map(int, input().strip().split()))
+    
+    d = [0] * n
+    d[0] = ls[0]
+
+    for i in range(n):
+        for j in range(i):
+            if ls[i] > ls[j]:
+                d[i] = max(d[i], d[j] + ls[i])
+            else:
+                d[i] = max(d[i], ls[i])
+    print(max(d))
 
 if __name__ == '__main__':
     main()
